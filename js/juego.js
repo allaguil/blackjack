b411cd5adbd371d043db1281ee9fa6edf4f024ff
Allan Aguilar <alan-joc@hotmail.com>
2022-10-30 00:01:21 -0600

    v3

--- js/juego.js
+++ js/juego.js
@@ -75,7 +75,7 @@ const miModulo = (() => {
     const crearCarta = (carta, turno) => {
         //Append Img to HTML
         const imgCarta = document.createElement('img');
-        imgCarta.src = `../assets/cartas/${carta}.png`;
+        imgCarta.src = `./cartas/${carta}.png`;
         imgCarta.classList.add('carta');
         divCartasJugadores[turno].append(imgCarta);
     }
